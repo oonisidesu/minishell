53b235bffdd6406e22a3f2bd0587306d7a77ebd8
Susumu Yagi <susumu.yagi@gmail.com>
2024-02-16 12:20:38 +0900

    プロンプトの表示を修正(readlineの引数に渡す)

--- src/paser/prompt.c
+++ src/paser/prompt.c
@@ -6,7 +6,7 @@
 /*   By: susumuyagi <susumuyagi@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 14:13:54 by susumuyagi        #+#    #+#             */
-/*   Updated: 2024/02/15 17:56:22 by susumuyagi       ###   ########.fr       */
+/*   Updated: 2024/02/16 12:18:53 by susumuyagi       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,7 @@ void	prompt(t_minishell *minish)
 	char	*buf;
 
 	(void)minish;
-	ft_putstr_fd(PROMPT, STDOUT_FILENO);
-	buf = readline(NULL);
+	buf = readline(PROMPT);
 	if (buf == NULL)
 	{
 		ft_putstr_fd(READ_ERR, STDERR_FILENO);
