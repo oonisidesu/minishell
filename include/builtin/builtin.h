4ed5af4b993a80d0d7e1776f59a6fceef164dcf9
oonisidesu <eeeelllleeee24@gmail.com>
2024-03-05 13:48:10 +0900

    builtin 関数をvoidからintに変更

--- include/builtin/builtin.h
+++ include/builtin/builtin.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: susumuyagi <susumuyagi@student.42.fr>      +#+  +:+       +#+        */
+/*   By: ootsuboyoshiyuki <ootsuboyoshiyuki@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 15:11:25 by susumuyagi        #+#    #+#             */
-/*   Updated: 2024/03/02 15:24:10 by susumuyagi       ###   ########.fr       */
+/*   Updated: 2024/03/05 13:47:26 by ootsuboyosh      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,27 @@
 # define BUILTIN_H
 
 # include "minishell.h"
+# include <unistd.h>
+# include <errno.h>
+# include <string.h>
 
 # define RUN_PARENT -2
 # define IS_BUILTIN(node) (lookup_builtin_func((node)->argv[0]) != NULL)
 # define IS_IN_PIPE(minish) ((minish)->node->next != NULL)
+# define PATHNAME_MAX 10000
+# define MINISHEEL_FRONT "minishell: "
+# define CD "cd: "
 
 typedef void	(*t_builtin_func)(t_minishell *minish, t_node *node);
 
 t_builtin_func	lookup_builtin_func(char *name);
 
-void			builtin_echo(t_minishell *minish, t_node *node);
-void			builtin_cd(t_minishell *minish, t_node *node);
-void			builtin_pwd(t_minishell *minish, t_node *node);
-void			builtin_export(t_minishell *minish, t_node *node);
-void			builtin_unset(t_minishell *minish, t_node *node);
-void			builtin_env(t_minishell *minish, t_node *node);
-void			builtin_exit(t_minishell *minish, t_node *node);
+int			builtin_echo(t_minishell *minish, t_node *node);
+int			builtin_cd(t_minishell *minish, t_node *node);
+int			builtin_pwd(t_minishell *minish, t_node *node);
+int			builtin_export(t_minishell *minish, t_node *node);
+int			builtin_unset(t_minishell *minish, t_node *node);
+int			builtin_env(t_minishell *minish, t_node *node);
+int			builtin_exit(t_minishell *minish, t_node *node);
 
 #endif
