40fbebf798fdae7972117f9ba583dd13cc93d39c
Susumu Yagi <susumu.yagi@gmail.com>
2024-03-05 12:35:40 +0900

    単体テストでエラーになるバグ修正

--- libft/ft_lstadd_front.c
+++ libft/ft_lstadd_front.c
@@ -6,16 +6,16 @@
 /*   By: susumuyagi <susumuyagi@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 19:07:30 by susumuyagi        #+#    #+#             */
-/*   Updated: 2023/05/31 13:20:25 by susumuyagi       ###   ########.fr       */
+/*   Updated: 2024/03/05 12:31:35 by susumuyagi       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd_front(t_list **lst, t_list *new)
+void	ft_lstadd_front(t_list **lst, t_list *new_lst)
 {
-	if (!lst || !new)
+	if (!lst || !new_lst)
 		return ;
-	new->next = *lst;
-	*lst = new;
+	new_lst->next = *lst;
+	*lst = new_lst;
 }
