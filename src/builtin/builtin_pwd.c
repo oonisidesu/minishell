cdba5a91ddd1755dcb98348c86963d8403574b7a
oonisidesu <eeeelllleeee24@gmail.com>
2024-03-07 16:52:00 +0900

    pwd修正 - pwd内のgetcwdが取得できた場合、minish->pwdに格納する ``` 権限ないディレクトリでpwd →エラー 権限付与してpwd →パス表示 権限ないディレクトリでpwd →パス表示

--- src/builtin/builtin_pwd.c
+++ src/builtin/builtin_pwd.c
@@ -6,7 +6,7 @@
 /*   By: ootsuboyoshiyuki <ootsuboyoshiyuki@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 19:23:01 by susumuyagi        #+#    #+#             */
-/*   Updated: 2024/03/07 16:32:38 by ootsuboyosh      ###   ########.fr       */
+/*   Updated: 2024/03/07 16:49:13 by ootsuboyosh      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,7 @@ int	builtin_pwd(t_minishell *minish, t_node *node)
 			node->wait_status = 1;
 			return (node->wait_status);
 		}
+		minish->pwd = pathname;
 	}
 	printf("%s\n", minish->pwd);
 	return (node->wait_status);
