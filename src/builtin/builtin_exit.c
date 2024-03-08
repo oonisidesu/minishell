2b9ef28aab9d8cfe4c7cb1b5e08ff076d82faeeb
oonisidesu <eeeelllleeee24@gmail.com>
2024-03-08 20:34:46 +0900

    pipeで繋がれた時にメッセージが表示されない 以外を対応

--- src/builtin/builtin_exit.c
+++ src/builtin/builtin_exit.c
@@ -6,15 +6,17 @@
 /*   By: ootsuboyoshiyuki <ootsuboyoshiyuki@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 19:23:01 by susumuyagi        #+#    #+#             */
-/*   Updated: 2024/03/06 18:51:50 by ootsuboyosh      ###   ########.fr       */
+/*   Updated: 2024/03/08 20:33:06 by ootsuboyosh      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
 #include "builtin/builtin.h"
 #include "libft.h"
-#include <stdlib.h>
+#include "minishell.h"
+#include <errno.h>
 #include <limits.h>
+#include <stdlib.h>
+#include <string.h>
 
 static char	*skip_space(char *str)
 {
@@ -85,37 +87,35 @@ int	builtin_exit(t_minishell *minish, t_node *node)
 {
 	if (node->argc == 1)
 		minish->status_code = 0;
-	if (is_numeric(node->argv[1]) && is_long_range(node->argv[1]))
+	else if (is_numeric(node->argv[1]) && is_long_range(node->argv[1]))
 	{
 		if (node->argc == 2)
 			minish->status_code = (unsigned char)ft_atoi(node->argv[1]);
 		else if (node->argc > 2)
 		{
-			ft_printf_fd(STDERR_FILENO, EXIT LINE_BREAK);
-			ft_printf_fd(STDERR_FILENO, MINISHEEL COLON EXIT COLON B_TOO_MANY_ARGS LINE_BREAK);
+			// TODO: pipeの時メッセージがうまく表示されない
+			if (!node->in_pipe)
+				ft_printf_fd(STDERR_FILENO, "exit\n");
+			ft_printf_fd(STDERR_FILENO, BUILTIN_ERROR_NOT_ARGS, "exit",
+				B_TOO_MANY_ARGS);
 			minish->status_code = 1;
 			return (minish->status_code);
 		}
 	}
 	else
 	{
-		if (node->argc == 2)
-		{
-			ft_printf_fd(STDERR_FILENO, EXIT LINE_BREAK);
-			ft_printf_fd(STDERR_FILENO, MINISHEEL COLON EXIT COLON "%s" COLON NUM_ARG_REQUIRED LINE_BREAK, node->argv[1]);
-			minish->status_code = 255;
-			exit(minish->status_code);
-		}
-		else 
-		{
-			ft_printf_fd(STDERR_FILENO, EXIT LINE_BREAK);
-			ft_printf_fd(STDERR_FILENO, MINISHEEL COLON EXIT COLON NUM_ARG_REQUIRED LINE_BREAK);
-			minish->status_code = 255;
-			return (minish->status_code);
-		}
+		// TODO: pipeの時メッセージがうまく表示されない
+		if (!node->in_pipe)
+			ft_printf_fd(STDERR_FILENO, "exit\n");
+		ft_printf_fd(STDERR_FILENO, BUILTIN_ERROR, "exit", node->argv[1],
+			NUM_ARG_REQUIRED);
+		minish->status_code = 255;
+		die_minishell(minish);
+		exit(minish->status_code);
 	}
 	die_minishell(minish);
-	printf(EXIT LINE_BREAK);
+	if (!node->in_pipe)
+		ft_printf_fd(STDOUT_FILENO, "exit\n");
 	exit(minish->status_code);
 	return (0);
 }
