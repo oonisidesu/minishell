9da93dbd72bd5924e6b4e006ae644730a57e4868
oonisidesu <eeeelllleeee24@gmail.com>
2024-03-11 16:10:17 +0900

    builtin echo 実装

--- src/builtin/builtin_echo.c
+++ src/builtin/builtin_echo.c
@@ -6,20 +6,67 @@
 /*   By: ootsuboyoshiyuki <ootsuboyoshiyuki@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 19:23:01 by susumuyagi        #+#    #+#             */
-/*   Updated: 2024/03/05 13:52:15 by ootsuboyosh      ###   ########.fr       */
+/*   Updated: 2024/03/11 16:04:17 by ootsuboyosh      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include "minishell.h"
 #include <stdlib.h>
 // TODO 後で消す
 #include <stdio.h>
+#include <unistd.h>
+
+static void	print_arg(t_node *node, int start)
+{
+	int		i;
+
+	i = start;
+	while (node->argv[i] != NULL)
+	{
+		ft_printf_fd(STDOUT_FILENO, "%s", node->argv[i]);
+		if (i + 1 < node->argc)
+			ft_printf_fd(STDOUT_FILENO, " ");
+		i++;
+	}
+	if (start == 1)
+		ft_printf_fd(STDOUT_FILENO, "\n");
+}
+
+static bool	is_option_n(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i == 0 && str[i] != '-')
+			return (false);
+		if (i == 1 && str[i] != 'n')
+			return (false);
+		if (i > 1 && str[i] != 'n')
+			return (false);
+		i++;
+	}
+	if (i == 1)
+		return (false);
+	return (true);
+}
 
 int	builtin_echo(t_minishell *minish, t_node *node)
 {
-	// TODO 後で実装する
+	int	start;
+
 	(void)minish;
+	start = 1;
 	node->wait_status = 0;
-	printf("builtin_echo\n");
-	return 0;
+	if (node->argc == 1)
+	{
+		ft_printf_fd(STDOUT_FILENO, "\n");
+		return (node->wait_status);
+	}
+	while (node->argc > start && is_option_n(node->argv[start]))
+		start++;
+	print_arg(node, start);
+	return (node->wait_status);
 }
