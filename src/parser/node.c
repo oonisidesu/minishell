344ed33d6aa965b35d41a7e633231a6e5d68fcaa
Susumu Yagi <susumu.yagi@gmail.com>
2024-04-01 18:02:45 +0900

    シェル変数複数宣言時にメモリリーク修正

--- src/parser/node.c
+++ src/parser/node.c
@@ -6,40 +6,13 @@
 /*   By: susumuyagi <susumuyagi@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 17:09:54 by susumuyagi        #+#    #+#             */
-/*   Updated: 2024/03/27 13:13:10 by susumuyagi       ###   ########.fr       */
+/*   Updated: 2024/04/01 17:57:50 by susumuyagi       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser/node.h"
 #include "utils/utils.h"
 #include <stdlib.h>
-
-static void	free_redirect(t_node *node)
-{
-	t_node	*tmp;
-
-	while (node)
-	{
-		tmp = node->next;
-		free_array((void **)node->argv);
-		free(node->path);
-		free(node);
-		node = tmp;
-	}
-}
-
-static void	free_declare(t_node *node)
-{
-	t_node	*tmp;
-
-	while (node)
-	{
-		tmp = node->redirect;
-		free_array((void **)node->argv);
-		free(node);
-		node = tmp;
-	}
-}
 
 void	free_nodes(t_node *node)
 {
@@ -48,8 +21,8 @@ void	free_nodes(t_node *node)
 	while (node)
 	{
 		tmp = node->next;
-		free_redirect(node->redirect);
-		free_declare(node->declare);
+		free_nodes(node->redirect);
+		free_nodes(node->declare);
 		free_array((void **)node->argv);
 		free(node->path);
 		free(node);
