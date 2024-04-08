1043a895af47fc48075ed769b6b9813bb59ef492
Susumu Yagi <susumu.yagi@gmail.com>
2024-04-08 15:38:22 +0900

    norm対応1(lexer.c)

--- src/parser/lexer.c
+++ src/parser/lexer.c
@@ -6,7 +6,7 @@
 /*   By: susumuyagi <susumuyagi@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 17:20:20 by susumuyagi        #+#    #+#             */
-/*   Updated: 2024/04/08 14:50:18 by susumuyagi       ###   ########.fr       */
+/*   Updated: 2024/04/08 15:37:22 by susumuyagi       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-static t_token	*new_token(t_minishell *minish, t_token_kind kind, t_token *cur,
-		char *str, size_t len)
+static void	new_token(t_minishell *minish, t_token_kind kind, char *str,
+		size_t len)
 {
 	t_token	*tok;
 
@@ -27,14 +27,14 @@ static t_token	*new_token(t_minishell *minish, t_token_kind kind, t_token *cur,
 	if (!tok)
 	{
 		occurred_malloc_error_return_null(minish);
-		return (NULL);
+		return ;
 	}
 	tok->kind = kind;
 	tok->str = str;
 	tok->len = len;
 	tok->next = NULL;
-	cur->next = tok;
-	return (tok);
+	minish->cur_token->next = tok;
+	minish->cur_token = tok;
 }
 
 static bool	is_starts_with(char *s1, char *s2, int len)
@@ -60,7 +60,6 @@ static ssize_t	count_word_len(char *p)
 	}
 	if (in_status != IN_NONE)
 	{
-		// "、'が閉じられていない
 		return (-1);
 	}
 	return (p - q);
@@ -88,11 +87,10 @@ int	tokenize(t_minishell *minish)
 {
 	char	*p;
 	t_token	head;
-	t_token	*cur;
 	ssize_t	word_len;
 
 	head.next = NULL;
-	cur = &head;
+	minish->cur_token = &head;
 	p = minish->line;
 	while (*p && no_error(minish))
 	{
@@ -103,27 +101,27 @@ int	tokenize(t_minishell *minish)
 		}
 		if (is_starts_with("<<", p, 2) || is_starts_with(">>", p, 2))
 		{
-			cur = new_token(minish, TK_RESERVED, cur, p, 2);
+			new_token(minish, TK_RESERVED, p, 2);
 			p += 2;
 			continue ;
 		}
 		if (is_reserved(*p))
 		{
-			cur = new_token(minish, TK_RESERVED, cur, p, 1);
+			new_token(minish, TK_RESERVED, p, 1);
 			p++;
 			continue ;
 		}
 		word_len = count_word_len(p);
 		if (word_len > 0)
 		{
-			cur = new_token(minish, TK_WORD, cur, p, word_len);
+			new_token(minish, TK_WORD, p, word_len);
 			p += word_len;
 			continue ;
 		}
 		return (error_at(minish, &head, p));
 	}
 	if (no_error(minish))
-		new_token(minish, TK_EOF, cur, p, 0);
+		new_token(minish, TK_EOF, p, 0);
 	minish->token = head.next;
 	return (!no_error(minish));
 }
