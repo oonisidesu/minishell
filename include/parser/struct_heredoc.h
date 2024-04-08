1d060a642fdaa9e72c95998dd070e7c1d5f08145
Susumu Yagi <susumu.yagi@gmail.com>
2024-04-08 14:20:00 +0900

    norm対応(strunc_heredoc.h)

--- include/parser/struct_heredoc.h
+++ include/parser/struct_heredoc.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   struct_heredoc.h                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ootsuboyoshiyuki <ootsuboyoshiyuki@stud    +#+  +:+       +#+        */
+/*   By: susumuyagi <susumuyagi@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 15:44:04 by susumuyagi        #+#    #+#             */
-/*   Updated: 2024/03/19 17:20:55 by ootsuboyosh      ###   ########.fr       */
+/*   Updated: 2024/04/08 14:19:25 by susumuyagi       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@
 # include <stddef.h>
 
 # define MAX_HEREDOC 16
+
 typedef struct s_heredoc
 {
 	size_t	num;
-
 	char	*delimiters[MAX_HEREDOC];
 	bool	need_expansion[MAX_HEREDOC];
 	char	*docs[MAX_HEREDOC];
